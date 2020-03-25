1fd98fa224747c0d6cc4b1611236fb679cabca10
FENGZHU\Haoku <fengzhuyiqiu@163.com>
2020-03-26 00:05:06 +0800

    添加代码注释

--- testemd.m
+++ testemd.m
@@ -3,5 +3,12 @@ t = 0:1/fs:10;
 x = sin(20*pi*t);
 y = 2*sin(1*pi*t);
 sig = x+y;
-plot(sig)
-PlotEMDandFFT(sig,fs)
+PlotEMDandFFT(sig,fs);  %该函数的源码获取方式见"代码说明.docx"
+
+% 关于函数： function imf = PlotEMDandFFT(y,Fs)
+% 信号EMD分解与各IMF分量频谱对照图
+% 输入：
+% y为待分解信号
+% Fs为采样频率
+% 输出：
+% imf为经emd分解后的各imf分量值，该值与直接调用emd函数返回的数值相同
